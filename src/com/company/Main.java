ab10cbea77fb48a4eb50a3ae499202e9e0c3eade
Conrad Chiu <conrockz007@gmail.com>
2019-05-22 20:57:45 -0400

    Create Main.java

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*Falssview Casino rules: 6 decks, 80% Pen and dealer stays on 17
+         */
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter how many decks (6 is at fallsview): ");
+        int deckNumber = input.nextInt();
+        for (int deckSize = 0; deckSize<=deckNumber; deckSize++)
+            System.out.println(deckSize);
+    }
+}
